d94ec6b595571d608325fe1644a026abffc4d9ba
Ahmed Hosni <ahhosni@vistaprint.com>
2019-12-03 13:49:01 +0100

    fix encoding

--- casino.py
+++ casino.py
@@ -1,23 +1,23 @@
-# Ce fichier abrite le code du ZCasino, un jeu de roulette adapté
+# Ce fichier abrite le code du ZCasino, un jeu de roulette adaptÃ©
 
 import os
 from random import randrange
 from math import ceil
 
-# Déclaration des variables de départ
-argent = 1000 # On a 1000 $ au début du jeu
-continuer_partie = True # Booléen qui est vrai tant qu'on doit
+# Dï¿½claration des variables de dÃ©part
+argent = 1000 # On a 1000 $ au dÃ©but du jeu
+continuer_partie = True # BoolÃ©en qui est vrai tant qu'on doit
                         # continuer la partie
 
-print("Vous vous installez à la table de roulette avec", argent, "$.")
+print("Vous vous installez Ã  la table de roulette avec", argent, "$.")
 
 while continuer_partie: # Tant qu'on doit continuer la partie
-    # on demande à l'utilisateur de saisir le nombre sur
+    # on demande Ã  l'utilisateur de saisir le nombre sur
     # lequel il va miser
     nombre_mise = -1
     while nombre_mise < 0 or nombre_mise > 49:
         nombre_mise = input("Tapez le nombre sur lequel vous voulez miser (entre 0 et 49) : ")
-        # On convertit le nombre misé
+        # On convertit le nombre misÃ©
         try:
             nombre_mise = int(nombre_mise)
         except ValueError:
@@ -25,11 +25,11 @@ while continuer_partie: # Tant qu'on doit continuer la partie
             nombre_mise = -1
             continue
         if nombre_mise < 0:
-            print("Ce nombre est négatif")
+            print("Ce nombre est nÃ©gatif")
         if nombre_mise > 49:
-            print("Ce nombre est supérieur à 49")
+            print("Ce nombre est supÃ©rieur Ã  49")
 
-    # À présent, on sélectionne la somme à miser sur le nombre
+    # Ã  prÃ©sent, on sÃ©lectionne la somme Ã  miser sur le nombre
     mise = 0
     while mise <= 0 or mise > argent:
         mise = input("Tapez le montant de votre mise : ")
@@ -41,38 +41,38 @@ while continuer_partie: # Tant qu'on doit continuer la partie
             mise = -1
             continue
         if mise <= 0:
-            print("La mise saisie est négative ou nulle.")
+            print("La mise saisie est nÃ©gative ou nulle.")
         if mise > argent:
             print("Vous ne pouvez miser autant, vous n'avez que", argent, "$")
 
-    # Le nombre misé et la mise ont été sélectionnés par
+    # Le nombre misÃ© et la mise ont Ã©tÃ© sÃ©lectionnÃ©s par
     # l'utilisateur, on fait tourner la roulette
     numero_gagnant = randrange(50)
-    print("La roulette tourne... ... et s'arrête sur le numéro", numero_gagnant)
+    print("La roulette tourne... ... et s'arrÃ©te sur le numÃ©ro", numero_gagnant)
 
-    # On établit le gain du joueur
+    # On Ã©tablit le gain du joueur
     if numero_gagnant == nombre_mise:
-        print("Félicitations ! Vous obtenez", mise * 3, "$ !")
+        print("FÃ©licitations ! Vous obtenez", mise * 3, "$ !")
         argent += mise * 3
-    elif numero_gagnant % 2 == nombre_mise % 2: # ils sont de la même couleur
+    elif numero_gagnant % 2 == nombre_mise % 2: # ils sont de la mÃªme couleur
         mise = ceil(mise * 0.5)
-        print("Vous avez misé sur la bonne couleur. Vous obtenez", mise, "$")
+        print("Vous avez misÃ© sur la bonne couleur. Vous obtenez", mise, "$")
         argent += mise
     else:
-        print("Désolé l'ami, c'est pas pour cette fois. Vous perdez votre mise.")
+        print("DÃ©solÃ© l'ami, c'est pas pour cette fois. Vous perdez votre mise.")
         argent -= mise
 
-    # On interrompt la partie si le joueur est ruiné
+    # On interrompt la partie si le joueur est ruinÃ©
     if argent <= 0:
-        print("Vous êtes ruiné ! C'est la fin de la partie.")
+        print("Vous Ãªtes ruinÃ© ! C'est la fin de la partie.")
         continuer_partie = False
     else:
         # On affiche l'argent du joueur
-        print("Vous avez à présent", argent, "$")
+        print("Vous avez Ã  prÃ©sent", argent, "$")
         quitter = input("Souhaitez-vous quitter le casino (o/n) ? ")
         if quitter == "o" or quitter == "O":
             print("Vous quittez le casino avec vos gains.")
             continuer_partie = False
 
-# On met en pause le système (Windows)
+# On met en pause le systÃªme (Windows)
 os.system("pause")
