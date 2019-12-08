397ff79bb05fee472fe718167e1f17dcc7586003
Ahmed Hosni <ahhosni@vistaprint.com>
2019-12-08 21:49:08 +0100

    add multithreading examples

---
+++ python-multithreading-run_function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+
+import threading
+import logging
+import time
+
+
+logging.basicConfig(
+	level=logging.DEBUG,
+	format = '(%(threadName)-10s) %(message)s',
+)
+
+
+class MyThread(threading.Thread):
+    def run(self):
+        logging.debug('This thread is running')
+        return
+
+for x in range(5):
+	z=MyThread()
+	z.start()
+	time.sleep(1)   
